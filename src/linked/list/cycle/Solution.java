e87040fc0952a2911025011fec1595ca4043b749
yeyuel <dx1988226@gmail.com>
2018-07-08 22:23:36 +0800

    Add cycle structure.

---
+++ src/linked/list/cycle/Solution.java
@@ -0,0 +1,11 @@
+package linked.list.cycle;
+
+import linked.list.ListNode;
+
+public class Solution {
+
+    // TODO
+    public boolean hasCycle(ListNode head) {
+        return false;
+    }
+}
---
+++ src/linked/list/cycle2/Solution.java
@@ -0,0 +1,11 @@
+package linked.list.cycle2;
+
+import linked.list.ListNode;
+
+public class Solution {
+
+    // TODO
+    public ListNode detectCycle(ListNode head) {
+        return null;
+    }
+}
