56e0aca1308e6d3eff4ea44dd8f38c9b340074bc
eric.ding <eric.ding@neulion.com.cn>
2018-07-09 18:38:17 +0800

    Add linked list cycle detection.

---
+++ src/linked/list/partition/Solution.java
@@ -0,0 +1,48 @@
+package linked.list.partition;
+
+import linked.list.ListNode;
+import linked.list.ListUtil;
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessHead = new ListNode(0);
+        ListNode moreHead = new ListNode(0);
+        ListNode lessPoint = lessHead;
+        ListNode morePoint = moreHead;
+        while (head != null ) {
+            if (head.val < x) {
+                lessPoint.next = head;
+                lessPoint = head;
+            } else {
+                morePoint.next = head;
+                morePoint = head;
+            }
+            head = head.next;
+        }
+        lessPoint.next = moreHead.next;
+        morePoint.next = null;
+        return lessHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(4);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(2);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(2);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+        ListUtil.printList(a);
+
+        Solution solution = new Solution();
+        ListNode partitioned = solution.partition(a, 3);
+
+        ListUtil.printList(partitioned);
+    }
+}
