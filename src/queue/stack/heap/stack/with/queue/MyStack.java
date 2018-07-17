784dc7b01a7b467b607573f829a487f2c131fa33
eric.ding <eric.ding@neulion.com.cn>
2018-07-17 15:18:39 +0800

    Add stack implementation with queue.

---
+++ src/queue/stack/heap/stack/with/queue/MyStack.java
@@ -0,0 +1,55 @@
+package queue.stack.heap.stack.with.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+
+    private Queue<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        Queue<Integer> tmpQueue = new LinkedList<>();
+        tmpQueue.add(x);
+        while (!queue.isEmpty())
+        {
+            tmpQueue.add(queue.poll());
+        }
+        while (!tmpQueue.isEmpty())
+        {
+            queue.add(tmpQueue.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.empty());
+    }
+}
