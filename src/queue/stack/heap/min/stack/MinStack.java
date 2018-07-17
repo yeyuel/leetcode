573b34ceeff7eb98b3d69b36e5b3c3ead0136427
eric.ding <eric.ding@neulion.com.cn>
2018-07-17 20:10:58 +0800

    Add min stack.

---
+++ src/queue/stack/heap/min/stack/MinStack.java
@@ -0,0 +1,48 @@
+package queue.stack.heap.min.stack;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    Stack<Integer> dataStack;
+    Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        dataStack.push(x);
+        if (!minStack.isEmpty() && x > minStack.peek()) {
+            minStack.push(minStack.peek());
+        } else {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
