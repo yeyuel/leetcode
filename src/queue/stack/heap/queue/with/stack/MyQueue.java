19f74c66b1c3b2206ccc8f6ee28cfeb4787cb0a3
eric.ding <eric.ding@neulion.com.cn>
2018-07-17 15:38:16 +0800

    Add queue with two stacks.

---
+++ src/queue/stack/heap/queue/with/stack/MyQueue.java
@@ -0,0 +1,59 @@
+package queue.stack.heap.queue.with.stack;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> inputStack;
+    private Stack<Integer> outputStack;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inputStack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+        return outputStack.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+        return outputStack.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return inputStack.isEmpty() &&
+                outputStack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        System.out.println(queue.peek());
+        System.out.println(queue.pop());
+        System.out.println(queue.empty());
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+        System.out.println(queue.empty());
+    }
+}
