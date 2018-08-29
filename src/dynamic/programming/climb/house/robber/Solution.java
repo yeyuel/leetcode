e79f9c9f890f27a4af0194df68e8f454e4af407f
eric.ding <eric.ding@neulion.com.cn>
2018-08-29 08:18:30 +0800

    Add climb house robber.

---
+++ src/dynamic/programming/climb/house/robber/Solution.java
@@ -0,0 +1,29 @@
+package dynamic.programming.climb.house.robber;
+
+public class Solution {
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] sample1 = {1,2,3,1};
+        int[] sample2 = {2,7,9,3,1};
+        System.out.println(solution.rob(sample1));
+        System.out.println(solution.rob(sample2));
+    }
+
+}
