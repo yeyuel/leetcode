fdfca685fa6362639f678218511851d3d724e5e1
yeyuel <dx1988226@gmail.com>
2018-08-30 21:55:41 +0800

    Add path sum.

---
+++ src/dynamic/programming/minimum/path/sum/Solution.java
@@ -0,0 +1,31 @@
+package dynamic.programming.minimum.path.sum;
+
+
+public class Solution {
+
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int column = grid[0].length;
+        int[][] dp = new int[row][column];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < column; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] sample = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(solution.minPathSum(sample));
+    }
+}
