c8c77fd6ce36536b4a197c532843081f5c6daa7d
yeyuel <dx1988226@hotmail.com>
2019-06-27 08:47:22 +0800

    Add length of longest substring.

---
+++ src/slide/window/longest/substring/Solution.java
@@ -0,0 +1,32 @@
+package slide.window.longest.substring;
+
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] sArr = s.toCharArray();
+        int[] hash = new int[256];
+
+        int l = 0, result = 1;
+
+        for (int r = 0; r < sArr.length; r++) {
+            hash[sArr[r]]++;
+
+            while (hash[sArr[r]] != 1) {
+                hash[sArr[l]]--;
+                l ++;
+            }
+            result = Math.max(result, r - l + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring(s));
+    }
+}
