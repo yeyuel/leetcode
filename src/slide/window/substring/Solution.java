dec2d2214cd0699cfa51003dfed8af684453f4da
yeyuel <dx1988226@gmail.com>
2019-06-29 17:24:13 +0800

    Add conclusion.

---
+++ src/slide/window/substring/Solution.java
@@ -0,0 +1,47 @@
+package slide.window.substring;
+
+
+public class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        char[] s1Arr = s1.toCharArray();
+        char[] s2Arr = s2.toCharArray();
+
+        int[] hash = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            hash[s1Arr[i] - 'a'] ++;
+        }
+        int l = 0, count = 0;
+        for (int r = 0; r < s2Arr.length; r++) {
+            hash[s2Arr[r] - 'a'] --;
+
+            if (hash[s2Arr[r] - 'a'] >= 0) {
+                count++;
+            }
+
+            if (r >= s2Arr.length) {
+                hash[s2Arr[l] - 'a']++;
+                if (hash[s2Arr[l] - 'a'] >= 1) {
+                    count --;
+                }
+                l++;
+            }
+            if (count == s1.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        Solution solution = new Solution();
+        System.out.println(solution.checkInclusion(s1, s2));
+    }
+}
