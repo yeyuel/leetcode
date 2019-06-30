05c4be8845c4b4cdf44798337139fa56bde69e73
yeyuel <dx1988226@gmail.com>
2019-06-30 17:36:16 +0800

    Add longest repeating replacement.

---
+++ src/slide/window/longest/repeating/Solution.java
@@ -0,0 +1,33 @@
+package slide.window.longest.repeating;
+
+public class Solution {
+
+    public int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] sArr = s.toCharArray();
+        int[] hash = new int[26];
+
+        int l = 0, maxCount = 0, result = 0;
+        for (int r = 0; r < sArr.length; r++) {
+            hash[sArr[r] - 'A']++;
+
+            maxCount = Math.max(maxCount, hash[sArr[r] - 'A']);
+
+            while (r - l + 1 - maxCount > k) {
+                hash[sArr[l] - 'A'] --;
+                l ++;
+            }
+            result = Math.max(r - l + 1, result);
+        }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        String input = "AABABBA";
+        int k = 1;
+        System.out.println(new Solution().characterReplacement(input, k));
+    }
+}
