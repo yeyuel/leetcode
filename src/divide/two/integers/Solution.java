e9a4548c8465c924288c25ed3c441a48e4e167f7
yeyuel <dx1988226@hotmail.com>
2019-08-06 20:35:13 +0800

    Update divide two integers.

--- src/divide/two/integers/Solution.java
+++ src/divide/two/integers/Solution.java
@@ -38,9 +38,32 @@ public class Solution {
         return negative ? 0 - res: res;
     }
 
+    public int divide1(int dividend, int divisor) {
+        int sign = (dividend ^ divisor) >> 31;
+        long lDividend = Math.abs((long) dividend);
+        long lDivisor = Math.abs((long) divisor);
+        long res = 0;
+        while (lDividend >= lDivisor) {
+            long tmp = lDivisor;
+            long i = 1;
+            while (lDividend >= tmp) {
+                lDividend -= tmp;
+                res += i;
+                i <<= 1;
+                tmp <<= 1;
+            }
+        }
+        if (sign == -1) res = -res;
+        if (res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else if (res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        return (int) res;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.divide(10, 3));
         System.out.println(solution.divide(7, -3));
+        System.out.println(solution.divide1(10, 3));
+        System.out.println(solution.divide1(7, -3));
     }
 }
