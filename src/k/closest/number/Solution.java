56934d8f61fb942ea6c4a2444483c36cfa41e0bd
yeyuel <dx1988226@gmail.com>
2019-10-07 22:17:53 +0800

    Update k closest number.

--- src/k/closest/number/Solution.java
+++ src/k/closest/number/Solution.java
@@ -29,10 +29,33 @@ public class Solution {
         return ret;
     }
 
+    public List<Integer> findClosestElements2(int[] arr, int k, int x) {
+        List<Integer> ret = new ArrayList<>();
+        if (arr == null || arr.length == 0) {
+            return ret;
+        }
+        int left = 0;
+        int right = arr.length - k;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        for (int i = left; i < left + k; i++) {
+            ret.add(arr[i]);
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         int[] input = {1, 2, 3, 4, 5};
         Solution solution = new Solution();
         System.out.println(solution.findClosestElements(input, 4, 3));
         System.out.println(solution.findClosestElements(input, 4, -1));
+        System.out.println(solution.findClosestElements2(input, 4, 3));
+        System.out.println(solution.findClosestElements2(input, 4, -1));
     }
 }
