d0ce633b9b25e9e900d9521347d1f7bf1810a265
yeyuel <dx1988226@hotmail.com>
2019-10-15 08:15:02 +0800

    Add another solution.

--- src/divide/conquer/search/rotated/sorted/array/two/Solution.java
+++ src/divide/conquer/search/rotated/sorted/array/two/Solution.java
@@ -40,10 +40,37 @@ public class Solution {
         return false;
     }
 
+    public boolean search1(int[] nums, int target) {
+        int begin = 0, end = nums.length - 1;
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[begin] == nums[mid]) {
+                begin ++;
+                continue;
+            } else if (nums[begin] < nums[mid]) {
+                if (target < nums[mid] && target >= nums[begin]) {
+                    end = mid - 1;
+                } else {
+                    begin = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    begin = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 5, 6, 0, 0, 1, 2};
         Solution solution = new Solution();
-        System.out.println(solution.search(nums, 0));
-        System.out.println(solution.search(nums, 3));
+        System.out.println(solution.search1(nums, 0));
+        System.out.println(solution.search1(nums, 3));
     }
 }
