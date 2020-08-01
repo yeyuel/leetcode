48df86fcd4982078029046df2671f1a8d9ac8058
yeyuel <dx1988226@gmail.com>
2020-08-01 23:02:49 +0800

    Add smallest range. Add slide window implementation.

--- src/small/range/Solution.java
+++ src/small/range/Solution.java
@@ -36,11 +36,58 @@ public class Solution {
         return new int[]{left, right};
     }
 
+    public int[] smallestRange1(List<List<Integer>> nums) {
+        int size = nums.size();
+        Map<Integer, List<Integer>> indices = new HashMap<>();
+        int xMin = Integer.MAX_VALUE, xMax = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            for (Integer x : nums.get(i)) {
+                List<Integer> list = indices.getOrDefault(x, new ArrayList<>());
+                list.add(i);
+                indices.put(x, list);
+                xMin = Math.min(xMin, x);
+                xMax = Math.max(xMax, x);
+            }
+        }
+        int[] freq = new int[size];
+        int inside = 0;
+        int left = xMin, right = xMin - 1;
+        int bestLeft = xMin, bestRight = xMax;
+        while (right < xMax) {
+            right ++;
+            if (indices.containsKey(right)) {
+                for (Integer x : indices.get(right)) {
+                    freq[x]++;
+                    if (freq[x] == 1) {
+                        inside++;
+                    }
+                }
+                while (inside == size) {
+                    if (right - left < bestRight - bestLeft) {
+                        bestLeft = left;
+                        bestRight = right;
+                    }
+                    if (indices.containsKey(left)) {
+                        for (int x : indices.get(left)) {
+                            freq[x]--;
+                            if (freq[x] == 0) {
+                                inside --;
+                            }
+                        }
+                    }
+                    left++;
+                }
+            }
+        }
+        return new int[]{bestLeft, bestRight};
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> nums = new ArrayList<>();
         nums.add(Arrays.asList(4, 10, 15, 24, 26));
         nums.add(Arrays.asList(0, 9, 12, 20));
         nums.add(Arrays.asList(5, 18, 22, 30));
         System.out.println(Arrays.toString(new Solution().smallestRange(nums)));
+        System.out.println(Arrays.toString(new Solution().smallestRange1(nums)));
     }
 }
