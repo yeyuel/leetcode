3013c4d38e3b7f0316a35e5770a8f71cf9437a05
eric.ding <eric.ding@neulion.com.cn>
2020-08-07 14:32:11 +0800

    Fix bug of next array implementation.

--- src/implement/str/Solution2.java
+++ src/implement/str/Solution2.java
@@ -2,7 +2,7 @@ package implement.str;
 
 public class Solution2 {
 
-    int kmp(String text, String pattern) {
+    private int kmp(String text, String pattern) {
         int i = 0, j = 0;
         int[] next = getNext(pattern);
         while (i < text.length() && j < pattern.length()) {
@@ -20,13 +20,13 @@ public class Solution2 {
         }
     }
 
-    int[] getNext(String pattern) {
+    private int[] getNext(String pattern) {
         int[] next = new int[pattern.length()];
         for (int i = 0; i < next.length; i++) {
             next[i] = -1;
         }
         int i = 0, j = -1;
-        while (i < pattern.length()) {
+        while (i + 1 < pattern.length()) {
             if (j == -1 || pattern.charAt(i) == pattern.charAt(j)) {
                 ++i;
                 ++j;
@@ -38,14 +38,26 @@ public class Solution2 {
         return next;
     }
 
+    public int strStr(String haystack, String needle) {
+        if(needle.equals("")) {
+            return 0;
+        }
+        if(haystack.equals("")) {
+            return -1;
+        }
+        return kmp(haystack, needle);
+    }
+
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
         String haystack1 = "hello", needle1 = "ll";
         String haystack2 = "aaaa", needle2 = "bba";
         String haystack3 = "mississippi", needle3 = "issipi";
-        System.out.println(solution.kmp(haystack1, needle1));
-        System.out.println(solution.kmp(haystack2, needle2));
-        System.out.println(solution.kmp(haystack3, needle3));
+        String haystack4 ="mississippi", needle4 = "issip";
+        System.out.println(solution.strStr(haystack1, needle1));
+        System.out.println(solution.strStr(haystack2, needle2));
+        System.out.println(solution.strStr(haystack3, needle3));
+        System.out.println(solution.strStr(haystack4, needle4));
     }
 
 }
