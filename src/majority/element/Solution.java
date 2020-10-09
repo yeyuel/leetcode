7a6a85f45be19794f63142db97264d54c0a9cb25
eric.ding <eric.ding@neulion.com.cn>
2020-10-09 11:25:12 +0800

    Add max product.

---
+++ src/majority/element/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 NeuLion, Inc. All Rights Reserved.
+ */
+package majority.element;
+
+/**
+ * Solution.
+ *
+ * @author <A HREF="mailto:eric.ding@neulion.com.cn">Eric.Ding</A>
+ * @version 1.0, $Revision: 0$, $Date: 2020/10/9$
+ * @since 1.0
+ */
+public class Solution
+{
+    
+}
--- src/mulitiply/max/sub/array/Solution.java
+++ src/mulitiply/max/sub/array/Solution.java
@@ -15,21 +15,17 @@ public class Solution
     public int maxProduct(int[] nums)
     {
         int length = nums.length;
-        int[] maxF = new int[length];
-        int[] minF = new int[length];
-        maxF[0] = nums[0];
-        minF[0] = nums[0];
+        int maxF = nums[0];
+        int minF = nums[0];
+        int ans = nums[0];
         for (int i = 1; i < length; i++)
         {
-            maxF[i] = Math.max(maxF[i - 1] * nums[i],
-                    Math.max(nums[i], minF[i - 1] * nums[i]));
-            minF[i] = Math.min(minF[i - 1] * nums[i],
-                    Math.min(nums[i], maxF[i - 1] * nums[i]));
-        }
-        int ans = maxF[0];
-        for (int i = 1; i < length; i++)
-        {
-            ans = Math.max(ans, maxF[i]);
+            int mx = maxF, mn = minF;
+            maxF = Math.max(mx * nums[i],
+                    Math.max(nums[i], mn * nums[i]));
+            minF = Math.min(mn * nums[i],
+                    Math.min(nums[i], mx * nums[i]));
+            ans = Math.max(maxF, ans);
         }
         return ans;
     }
