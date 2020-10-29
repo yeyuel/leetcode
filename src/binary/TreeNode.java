6ba79ac53a068a415a6a1c2fa117219ecdb7bd15
yeyuel <dx1988226@gmail.com>
2020-10-29 08:54:29 +0800

    add kth smallest bst.

--- src/binary/TreeNode.java
+++ src/binary/TreeNode.java
@@ -6,6 +6,12 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode(int x) { val = x;}
 
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
     @Override
     public String toString() {
         return String.valueOf(val);
