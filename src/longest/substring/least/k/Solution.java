4c5718f11e5b72db46c3e522de0aeb38085a7274
yeyuel <dx1988226@gmail.com>
2020-11-06 08:43:39 +0800

    Add at least k duplicate substring.

---
+++ src/longest/substring/least/k/Solution.java
@@ -0,0 +1,16 @@
+package longest.substring.least.k;
+
+public class Solution {
+
+    public int longestSubstring(String s, int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String input1 = "aaabb";
+        String input2 = "ababbc";
+        Solution solution = new Solution();
+        System.out.println(solution.longestSubstring(input1, 3));
+        System.out.println(solution.longestSubstring(input2, 2));
+    }
+}
