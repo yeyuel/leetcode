aa43532663d301feb452e6582067254acdf7b5c3
eric.ding <eric.ding@neulion.com.cn>
2020-11-12 13:39:50 +0800

    Add official solution.

---
+++ src/breadth/first/search/word/ladder/Solution1.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020 NeuLion, Inc. All Rights Reserved.
+ */
+package breadth.first.search.word.ladder;
+
+import java.util.*;
+
+
+/**
+ * Solution1.
+ *
+ * @author <A HREF="mailto:eric.ding@neulion.com.cn">Eric.Ding</A>
+ * @version 1.0, $Revision: 0$, $Date: 2020/11/12$
+ * @since 1.0
+ */
+public class Solution1
+{
+    Map<String, Integer> wordId = new HashMap<>();
+    List<List<Integer>> edge = new ArrayList<>();
+    int nodeNum = 0;
+
+    public int ladderLength(String beginWord, String endWord,
+            List<String> wordList)
+    {
+        for (String word : wordList)
+        {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.containsKey(endWord))
+        {
+            return 0;
+        }
+        int[] dis = new int[nodeNum];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+        dis[beginId] = 0;
+
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(beginId);
+        while (!que.isEmpty())
+        {
+            int x = que.poll();
+            if (x == endId)
+            {
+                return dis[endId] / 2 + 1;
+            }
+            for (Integer it : edge.get(x))
+            {
+                if (dis[it] == Integer.MAX_VALUE)
+                {
+                    dis[it] = dis[x] + 1;
+                    que.offer(it);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void addEdge(String word)
+    {
+        addWord(word);
+        int id1 = wordId.get(word);
+        char[] array = word.toCharArray();
+        int length = array.length;
+        for (int i = 0; i < length; i++)
+        {
+            char tmp = array[i];
+            array[i] = '*';
+            String newWord = new String(array);
+            addWord(newWord);
+            int id2 = wordId.get(newWord);
+            edge.get(id1).add(id2);
+            edge.get(id2).add(id1);
+            array[i] = tmp;
+        }
+    }
+
+    public void addWord(String word)
+    {
+        if (!wordId.containsKey(word))
+        {
+            wordId.put(word, nodeNum++);
+            edge.add(new ArrayList<>());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String[] wordList1 = new String[] { "hot", "dot", "dog", "lot", "log",
+                "cog" };
+        String[] wordList2 = new String[] { "hot", "dot", "dog", "lot", "log" };
+        System.out.println(new Solution1().ladderLength("hit", "cog", Arrays.asList(wordList1)));
+        System.out.println(new Solution1().ladderLength("hit", "cog", Arrays.asList(wordList2)));
+    }
+}
