9b45b3e16cab5568f3d0d334fb165cddec7c3289
yeyuel <dx1988226@gmail.com>
2020-11-17 08:43:42 +0800

    Add is palindrome.

---
+++ src/is/palindrome/Solution.java
@@ -0,0 +1,32 @@
+package is.palindrome;
+
+public class Solution {
+
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(solution.isPalindrome("race a car"));
+    }
+}
