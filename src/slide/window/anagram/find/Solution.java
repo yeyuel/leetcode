2c41230dfd1f1fddd48476d920e5d81db4035c84
yeyuel <dx1988226@gmail.com>
2022-01-03 10:24:09 +0800

    Add solution more understandable.

--- src/slide/window/anagram/find/Solution.java
+++ src/slide/window/anagram/find/Solution.java
@@ -5,6 +5,41 @@ import java.util.List;
 
 public class Solution {
 
+    public List<Integer> findAnagramsNew(String s, String p) {
+        List<Integer> results = new ArrayList<>();
+        if (s.length() < p.length()) return results;
+
+        char[] sArr = s.toCharArray();
+        char[] pArr = p.toCharArray();
+
+        int[] need = new int[26];
+        for (char value : pArr) {
+            need[value - 'a']++;
+        }
+        int[] window = new int[26];
+
+        int left = 0, right = 0, valid = 0;
+        while (right < sArr.length) {
+            char c = sArr[right];
+            right++;
+
+            if (need[c - 'a'] > 0) {
+                window[c - 'a']++;
+                if (window[c - 'a'] == need[c - 'a']) valid ++;
+            }
+            while (right - left >= pArr.length) {
+                if (valid == pArr.length) results.add(left);
+                char d = sArr[left];
+                left ++;
+                if (need[d - 'a'] > 0) {
+                    if (window[d - 'a'] == need[d - 'a']) valid--;
+                    window[d - 'a']--;
+                }
+            }
+        }
+        return results;
+    }
+
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> results = new ArrayList<>();
         if (s.length() < p.length()) {
@@ -47,7 +82,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        List<Integer> anagrams = solution.findAnagrams("sdfsdiifsdfjsjdhfsdf", "sd");
+        List<Integer> anagrams = solution.findAnagramsNew("sdfsdiifsdfjsjdhfsdf", "sdf");
         System.out.println(anagrams.toString());
     }
 }
