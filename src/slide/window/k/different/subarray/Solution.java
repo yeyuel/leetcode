bcc19f0f315b7ed0b988e781d13b2bd9462e9d6f
yeyuel <dx1988226@gmail.com>
2022-01-03 21:05:45 +0800

    Add solution more understandable.

--- src/slide/window/k/different/subarray/Solution.java
+++ src/slide/window/k/different/subarray/Solution.java
@@ -1,6 +1,36 @@
 package slide.window.k.different.subarray;
 
+import java.util.Arrays;
+
 public class Solution {
+
+    public int subarraysWithKDistinctNew(int[] A, int K) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int[] hash = new int[A.length + 1];
+        int l = 0, r = 0, valid = 0, results = 0;
+        while (r < A.length) {
+            int c = A[r];
+            if (++hash[c] == 1) valid ++;
+
+            while (valid >= K) {
+                int d = A[l];
+                if (valid == K) {
+                    results++;
+                }
+                l ++;
+
+                if (hash[d] > 0) {
+                    if (--hash[d] == 0) {
+                        valid--;
+                    }
+                }
+            }
+            r++;
+        }
+        return results;
+    }
 
     public int subarraysWithKDistinct(int[] A, int K) {
         if (A == null || A.length == 0) {
@@ -39,6 +69,6 @@ public class Solution {
         int k = 3;
 
         Solution solution = new Solution();
-        System.out.println(solution.subarraysWithKDistinct(a, k));
+        System.out.println(solution.subarraysWithKDistinctNew(a, k));
     }
 }
