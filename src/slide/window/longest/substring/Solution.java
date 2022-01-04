33b1e0a04e2e78870c6e2f5dbcd4ea20843c11c7
yeyuel <dx1988226@gmail.com>
2022-01-04 09:00:58 +0800

    Make code more understandable.

--- src/slide/window/longest/substring/Solution.java
+++ src/slide/window/longest/substring/Solution.java
@@ -2,6 +2,29 @@ package slide.window.longest.substring;
 
 public class Solution {
 
+    private int lengthOfLongestSubstringNew(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] sArr = s.toCharArray();
+        int[] hash = new int[26];
+        int l = 0, r = 0, max = 0;
+        while (r < sArr.length) {
+            char c = sArr[r];
+            r++;
+
+            hash[c - 'a']++;
+
+            while (hash[c - 'a'] > 1) {
+                char d = sArr[l];
+                if (hash[d - 'a'] > 0) hash[d - 'a']--;
+                l++;
+            }
+            max = Math.max(max, r - l);
+        }
+        return max;
+    }
+
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) {
             return 0;
@@ -27,6 +50,6 @@ public class Solution {
     public static void main(String[] args) {
         String s = "abcabcbb";
         Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring(s));
+        System.out.println(solution.lengthOfLongestSubstringNew(s));
     }
 }
