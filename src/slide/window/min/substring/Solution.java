b78b00e052bce99914fd000b7753693258f8986e
yeyuel <dx1988226@gmail.com>
2022-01-05 23:01:39 +0800

    Make code more understandable.

--- src/slide/window/min/substring/Solution.java
+++ src/slide/window/min/substring/Solution.java
@@ -38,11 +38,48 @@ public class Solution {
         return result;
     }
 
+    private String minWindowNew(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        int[] window = new int[26];
+        int[] need = new int[26];
+
+        int targetValid = 0;
+        for (char c : tArr) {
+            if (++need[c - 'A'] == 1) targetValid++;
+        }
+
+        int l = 0, r = 0, valid = 0, max = sArr.length + 1;
+        String result = "";
+        while (r < sArr.length) {
+            char c = sArr[r];
+            r++;
+
+            if (++window[c - 'A'] == need[c - 'A']) valid++;
+
+            while (valid == targetValid) {
+                if (r - l < max) {
+                    max = r - l;
+                    result = s.substring(l, r);
+                }
+                char d = sArr[l];
+                l++;
+
+                if (--window[d - 'A'] < need[d - 'A']) valid--;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
 
         Solution solution = new Solution();
-        System.out.println(solution.minWindow(s, t));
+//        System.out.println(solution.minWindow(s, t));
+        System.out.println(solution.minWindowNew(s, t));
     }
 }
