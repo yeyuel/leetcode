8f66628e59f206f3a2aabf210ceefca9bfd99b10
yeyuel <dx1988226@gmail.com>
2022-01-05 09:25:51 +0800

    Make code more understandable.

--- src/slide/window/substring/inclusion/Solution.java
+++ src/slide/window/substring/inclusion/Solution.java
@@ -38,10 +38,41 @@ public class Solution {
         return false;
     }
 
+    private boolean checkInclusionNew(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        char[] s1Arr = s1.toCharArray();
+        char[] s2Arr = s2.toCharArray();
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+
+        for (char c : s1Arr) {
+            need[c - 'a']++;
+        }
+
+        int l = 0, r = 0, valid = 0;
+        while (r < s2Arr.length) {
+            char c = s2Arr[r];
+            r ++;
+
+            if (++window[c - 'a'] == need[c - 'a']) valid++;
+
+            while (r - l > s1Arr.length) {
+                char d = s2Arr[l];
+                l++;
+                if (window[d - 'a'] > 0){
+                    if (--window[d - 'a'] < need[d - 'a']) valid--;
+                }
+            }
+            if (valid == s1Arr.length) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         String s1 = "bd";
         String s2 = "eidbaooo";
         Solution solution = new Solution();
-        System.out.println(solution.checkInclusion(s1, s2));
+        System.out.println(solution.checkInclusionNew(s1, s2));
     }
 }
